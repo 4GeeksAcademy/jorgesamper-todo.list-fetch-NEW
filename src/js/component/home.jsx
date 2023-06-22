6325677b1f4898c5fc3424b22e3f4ca7e36e7caa
JSamper <60151922+jorgesamper@users.noreply.github.com>
2023-06-22 12:50:15 +0200

    Finished with fetch and API

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,55 +1,96 @@
 import React from "react";
 
 
-//create your first component
+// Componente de inicio
 const Home = () => {
 	// Crear un estado
 	// const [estado, setter] = React.useState(estadoInicial)
-	const [todoList, setTodoList] = React.useState([{ label: "Elemento 1", done: false }])
-	const [inputValue, setInputValue] = React.useState("")
-	
 
-	// Añadir nuevo elemento
+
+	const [todoList, setTodoList] = React.useState([])
+	const [inputValue, setInputValue] = React.useState("")
+	const API_URL = "https://assets.breatheco.de/apis/fake/todos/user/jorgesamper"
+
+	// Función para agregar una nueva tarea a la lista
 	const handleAddNewTodo = () => {
 		setTodoList(prev => [...prev, { label: inputValue, done: false }])
 	}
 
-	// Eliminar elemento
+	// Función para eliminar una tarea de la lista
 	const handleDeleteTodo = (indexToRemove) => {
 		setTodoList(prev => prev.filter((element, index) => index !== indexToRemove))
 	}
 
-	// Cuando le damos al enter salta esta función
+	// Función para manejar la pulsación de teclas en el campo de entrada
 	const handleOnKeyDown = (e) => {
-		// Conseguir que tecla se ha presionado
-		const keyPressed = e.key
+		const keyPressed = e.key // Obtener la tecla presionada
 
-		// Miramos que inputValue no esté vacío y que la tecla sea el enter
+		
 		if (inputValue && keyPressed === "Enter") {
-			// Añadimos un nuevo elemento
-			handleAddNewTodo()
-			// Limpiamos el unput Volvemos a poner input value como vacío
-			setInputValue("")
+			// Si el campo de entrada no está vacío y se presionó la tecla Enter
+			handleAddNewTodo(); // Agregar una nueva tarea
+			setInputValue(""); // Limpiar el campo de entrada
 		}
 	}
 
-	return (
-		<div className="text-center">
-			<h1>Todo list</h1>
-			<hr />
-			<input onKeyDown={handleOnKeyDown} value={inputValue} onChange={(e) => setInputValue(e.target.value)} />
-			{
-				todoList.map((singleTodo, index) => {
-					return <ul style={{ margin: "1rem" }}>
-						<section style={{ display: "flex", justifyContent: "space-between" }}>
-							<li key={index}>{singleTodo.label}</li>
-							<button onClick={() => handleDeleteTodo(index)}>{`X ${singleTodo.label}`}</button>
-						</section>
+	const handleDeleteAll = () => {
+		setTodoList([]); // Establecer una lista vacía
+	  };
 
-					</ul>
-				})
-				// 
+
+	// Efecto que se ejecuta una vez al cargar el componente
+	React.useEffect(() => {
+		// Hacer una solicitud a la API para obtener la lista de tareas
+		fetch(API_URL)
+			.then(promiseResponse => {
+				if (promiseResponse.ok)
+					return promiseResponse.json().then(data => setTodoList(data))
+
+				// Si no hay ningún usuario creado lo creamos
+				const createUserOptions = {
+					method: "POST",
+					body: JSON.stringify([]),
+					headers: { "Content-Type": "application/json" }
+				}
+				fetch(API_URL, createUserOptions)
+
+			})
+
+	}, [])
+
+	// Efecto que se ejecuta cuando el estado `todoList` cambia
+	React.useEffect(() => {
+
+		if (todoList.length > 0) {
+			// Si la lista de tareas no está vacía, actualizar la lista en la API
+			const udpateTodoOptions = {
+				method: "PUT",
+				body: JSON.stringify(todoList),
+				headers: { "Content-Type": "application/json" }
 			}
+			fetch(API_URL, udpateTodoOptions)
+		}
+
+	}, [todoList])
+
+	// Renderizado del componente
+	return (
+		<div className="text-center bg-light mx-3 shadow-sm">
+			<h1 className="mt-3">TodoList!</h1>
+			<hr />
+			<input className="border w-100  text-center" placeholder="Write your first task . . ." onKeyDown={handleOnKeyDown} value={inputValue} onChange={(e) => setInputValue(e.target.value)} />
+			<ul style={{ margin: "1rem" }}>
+				{
+					todoList.map((singleTodo, index) => {
+						return <section key={index} style={{ display: "flex", justifyContent: "space-between" }}>
+							<li >{singleTodo.label}</li>
+							<button className="btn" onClick={() => handleDeleteTodo(index)}>{`X`}</button>
+						</section>
+					})
+				}
+			</ul>
+			<span className="fw-bold text-primary" >TOTAL Nº {todoList.length}</span>
+			<button className="btn text-danger " onClick={handleDeleteAll} >Delete all</button>
 		</div>
 	);
 };
